18319bcdad0778140ec4ab97a525d241723f438a
Santosh Wadghule <santosh.wadghule@gmail.com>
2010-07-25 13:07:04 +0530

    Rails 3 app

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,71 +4,63 @@ include REXML
 
 
 class MapsController < ApplicationController
-	def index
-	@name = "City Eatouts";
-	@contact = "santosh.wadghule@gmail.com";
-	@maplink = locateIp();
-	
-	
+  def index
+  	@name = "City Eatouts"
+  	@contact = "santosh.wadghule at gmail dot com"
+  	@maplink = locateIp()
+  end
+
+  def locateIp
+    if RAILS_ENV == "development"
+  	  # ip = request.remote_ip;
+  		# ips = ip.to_s;
+  	  ips = "123.236.183.15"
+    else
+  	  #ips = "123.236.183.15";
+  		ip = request.remote_ip
+  		ips = ip.to_s
     end
+  		
+  		url = "http://ipinfodb.com/ip_query.php?ip="+ips+"&timezone=false"
+      xml_data = Net::HTTP.get_response(URI.parse(url)).body
+      xmldoc = REXML::Document.new(xml_data)
 
-	def locateIp
-		if RAILS_ENV == "development"
-			# ip = request.remote_ip;
-			# ips = ip.to_s;
-		ips = "123.236.183.15"
-		
-	   
-	    else
-	  #ips = "123.236.183.15";
-		ip = request.remote_ip;
-		ips = ip.to_s;
-		
-	end
-		url = "http://iplocationtools.com/ip_query.php?ip="+ips;
-		
-		xml_data = Net::HTTP.get_response(URI.parse(url)).body
+      # Now get the root element	
+  		root = xmldoc.root
+  		city = ""
+  		regionName = ""
+  		countryName = ""
 
-        xmldoc = REXML::Document.new(xml_data)
+  		# This will take country name...
+  		xmldoc.elements.each("Response/CountryName") {
+  		|e| countryName << e.text 
+  	    }
 
 
-		# Now get the root element	
-		root = xmldoc.root
-		city = ""
-		regionName = ""
-		countryName = ""
-		
-		# This will take country name...
-		xmldoc.elements.each("Response/CountryName") {
-		|e| countryName << e.text 
-	    }
-   		
-       
-		# Now get city name...
-		xmldoc.elements.each("Response/City") {
-   		|e| city << e.text 
-		}
+  		# Now get city name...
+  		xmldoc.elements.each("Response/City") {
+     		|e| city << e.text 
+  		}
 
-		# This will take regionName...
-		xmldoc.elements.each("Response/RegionName") {
-   		|e| regionName << e.text 
-	    }
-     ipLocation = city +", "+regionName+", "+countryName
-	
-	 # stored city name in intance variable...
-	 @city = city
-	 return ipLocation
-   end #end of method locateIp
-	
-	
-	def about
-	end
-	
-	def share
-	end
-	
-	def new
-	end
+  		# This will take regionName...
+  		xmldoc.elements.each("Response/RegionName") {
+     		|e| regionName << e.text 
+  	    }
+       ipLocation = city +", "+regionName+", "+countryName
 
+  	 # stored city name in intance variable...
+  	 @city = city
+  	 return ipLocation
+     end #end of method locateIp
+
+
+  	def about
+  	end
+
+  	def share
+  	end
+
+  	def new
+  	end
 
 end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'ab07cc31cc302fa71d3dc4741e033ff73beff17f9324844b570a99bba1da28deb82a2e0435ecce44e7fba04e00fa0f72325143f09781139824618826f42d3202'
